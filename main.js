5055fc7f251b1140a4a88daf321379d647505870
kobayuku <dominik.kielbowicz19@gmail.com>
2022-05-26 15:50:25 +0200

    init

--- main.js
+++ main.js
@@ -28,20 +28,7 @@ const getGameId = (allGames, gameName) => {
     return false;
 }
 
-const areEqual = (obj1, obj2) => {
-    console.log("Sprawdzam.");
-    const firstKeys = Object.keys(obj1);
-    const secondKeys = Object.keys(obj2);
-    if (firstKeys.length !== secondKeys.length) {
-        return false;
-    }
-
-    if (firstKeys.some((key, index) => obj1[key] !== obj2[secondKeys[index]])) {
-        return false;
-    }
-
-    return true;
-}
+const isPriceLower = (price1, price2) => price1 < price2;
 
 const createEmbed = object => {
     const { name, rating, price, originalPrice, discount, metacritic, gameUrl, image } = object;
@@ -59,38 +46,43 @@ const createEmbed = object => {
 }
 
 const getDeals = async () => {
-    console.log(new Date().toLocaleTimeString());
-    const file = JSON.parse(fs.readFileSync("deals.json"));
-    const request = await axios.get(URL);
-    const data = request.data;
-    const DOM = new jsdom.JSDOM(data).window.document;
-    const allDeals = DOM.querySelectorAll(".deal-list-item");
-    const allGames = await steam.getAppList();
-    const gamesEmbeds = []
-    for (const deal of allDeals) {
-        const name = deal.querySelector(".title-line a").textContent;
-        const image = deal.querySelector(".main-image img").getAttribute("src");
-        const id = String(getGameId(allGames, name));
-        const details = await steam.getGameDetails(id);
-        const metacritic = details.metacritic?.score || 0;
-        const rating = deal.querySelector(".rating-badge").textContent;
-        const [price, originalPrice] = Array.from(deal.querySelectorAll(".price .double-line span")).map(el => el.textContent);
-        const discount = deal.querySelector(".discount-badge").textContent;
-        const gameUrl = `https://store.steampowered.com/app/${id}/${name.replace(/ /g, "_")}/`;
-        const gameObject = new Game(name, rating, price, originalPrice, discount, metacritic, gameUrl, image);
-        if (!file.hasOwnProperty(name) || !areEqual(file[name], gameObject)) {
-            file[name] = gameObject;
-            gamesEmbeds.push(createEmbed(gameObject));
-        };
+    try {
+        console.log(new Date().toLocaleTimeString());
+        const file = JSON.parse(fs.readFileSync("deals.json"));
+        const request = await axios.get(URL);
+        const data = request.data;
+        const DOM = new jsdom.JSDOM(data).window.document;
+        const allDeals = DOM.querySelectorAll(".deal-list-item");
+        const allGames = await steam.getAppList();
+        const gamesEmbeds = [];
+        for (const deal of allDeals) {
+            const name = deal.querySelector(".title-line a").textContent;
+            const image = deal.querySelector(".main-image img").getAttribute("src");
+            const id = String(getGameId(allGames, name));
+            console.log(id);
+            const details = await steam.getGameDetails(id);
+            const metacritic = details.metacritic?.score || 0;
+            const rating = deal.querySelector(".rating-badge").textContent;
+            const [price, originalPrice] = Array.from(deal.querySelectorAll(".price .double-line span")).map(el => el.textContent);
+            const discount = deal.querySelector(".discount-badge").textContent;
+            const gameUrl = `https://store.steampowered.com/app/${id}/${name.replace(/ /g, "_")}/`;
+            const gameObject = new Game(name, rating, price, originalPrice, discount, metacritic, gameUrl, image);
+            if (!file.hasOwnProperty(name) || isPriceLower(price, file[name].price)) {
+                file[name] = gameObject;
+                gamesEmbeds.push(createEmbed(gameObject));
+            };
+        }
+        if (gamesEmbeds.length) {
+            const WH = "https://discordapp.com/api/webhooks/746080172603605012/n4eTp71gRtt5M7ZDdGbgDVpEy8uCMX1fEdQPjyJPJjFVb5fCmpRdDZVo3aYzSMfOYTKS";
+            await axios.post(WH, {
+                embeds: gamesEmbeds
+            });
+            gamesEmbeds.length = 0;
+        }
+        fs.writeFileSync("deals.json", JSON.stringify(file));
+    } catch (e) {
+        console.log(e);
     }
-    if (gamesEmbeds.length) {
-        const WH = "https://discordapp.com/api/webhooks/746080172603605012/n4eTp71gRtt5M7ZDdGbgDVpEy8uCMX1fEdQPjyJPJjFVb5fCmpRdDZVo3aYzSMfOYTKS";
-        await axios.post(WH, {
-            embeds: gamesEmbeds
-        });
-        gamesEmbeds.length = 0;
-    }
-    fs.writeFileSync("deals.json", JSON.stringify(file));
 }
 
 class Game {
@@ -106,5 +98,7 @@ class Game {
     }
 }
 
-const job = new cron('* 10,30,50 * * * *', getDeals);
+const job = new cron('00 10,30,50 * * * *', getDeals);
 job.start();
+
+getDeals()
